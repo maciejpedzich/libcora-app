964fb5a6e8f76ca2580a92a56f02afffa2f0c971
Maciej Pędzich <dnalop.me@gmail.com>
2021-05-05 12:27:28 +0200

    Extract header and page markup from "Folder.vue"

---
+++ src/components/ui/Page.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button color="primary"></ion-menu-button>
+        </ion-buttons>
+        <ion-title>{{ title }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <slot></slot>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonPage,
+  IonButtons,
+  IonHeader,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+  IonContent
+} from '@ionic/vue';
+
+export default {
+  name: 'Page',
+  props: ['title'],
+  components: {
+    IonPage,
+    IonButtons,
+    IonHeader,
+    IonMenuButton,
+    IonTitle,
+    IonToolbar,
+    IonContent
+  }
+};
+</script>
