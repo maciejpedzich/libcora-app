a81ba631345a814bbb41b17c78cf8f0f82aed6ac
Maciej Pędzich <dnalop.me@gmail.com>
2021-05-05 12:46:38 +0200

    Create placeholder Home page

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <Page title="Home">
+    <p>
+      Welcome home
+    </p>
+  </Page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Page from '@/components/ui/Page.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Page
+  }
+});
+</script>
