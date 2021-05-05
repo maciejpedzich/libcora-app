d6d432d0628ca9ff395f36d242dd57ac7100f956
Maciej Pędzich <dnalop.me@gmail.com>
2021-05-05 12:39:40 +0200

    Create constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,25 @@
+const TEN_MINUTES = 6e5;
+const BOOK_GENRES = [
+  'Fantasy',
+  'Adventure',
+  'Romance',
+  'Dystopian',
+  'Mystery',
+  'Horror',
+  'Thriller',
+  'Paranormal',
+  'Historical Fiction',
+  'Science Fiction',
+  'Memoir',
+  'Cooking',
+  'Art',
+  'Self-help / Personal',
+  'Health',
+  'History',
+  'Travel',
+  'Guide / How-to',
+  'Families & Relationships',
+  'Humour'
+];
+
+export { TEN_MINUTES, BOOK_GENRES };
